23963cbedf5a5e6b294d320080bb5a9c18340a9d
Calvin Ho <calvin.ch.ho@gmail.com>
2022-08-07 14:12:47 -0400

    Setup react router with initial route to letters

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import "./styles/letterItem.scss"  
+import logo from "./logo.svg";
+import { Route, Link, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import "./styles/letterItem.scss";
 
-import { LetterList } from './components/LetterList';
+import { LetterList } from "./components/LetterList";
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <div className="App">
-      <LetterList />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <button
+              onClick={() => {
+                navigate("/letters");
+              }}
+            >
+              All Letters{" "}
+            </button>
+          }
+        />
+        <Route path="/letters" element={<LetterList />} />
+      </Routes>
     </div>
   );
 }
