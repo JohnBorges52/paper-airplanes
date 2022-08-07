12e2a3af12214e999e59e1574205b1942b69978b
Derine Banis <d.banis.13@gmail.com>
2022-08-06 21:12:50 -0700

    Set up front end for letter cards

--- client/src/components/Letter_Card.jsx
+++ client/src/components/Letter_Card.jsx
@@ -1,7 +1,19 @@
+import React from "react"
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+
 export const Letter_Card = () => {
+  const  [data, setData] = useState(['steven', 'charles'])
+  useEffect(()=>{
+    axios.get('/letters')
+      .then(res => setData(res.data)) //data =
+      // .then(data_from_above => setData(data_from_above))
+  }, [])
   return (
     <div>
-      
+      {data.map(letter => <p key="{letter.id}" >{letter.letter_message}</p>)}
     </div>
   )
+
 }
