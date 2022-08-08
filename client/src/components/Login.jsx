f07f794934eb05a5597e81b77f3c2ba5ae0217ac
Derine Banis <d.banis.13@gmail.com>
2022-08-07 22:51:53 -0700

    Add logic to allow changing user based on email

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,6 +4,7 @@ import { UserContext } from "../UserContext";
 import { useContext } from "react";
 
 export const Login = () => {
+  const[userEmail, setUserEmail] = useState("")
   const{userID, setUserID} = useContext(UserContext);
   const getUserId =  (email) => {
     axios.post('/users/login', {email})
@@ -17,9 +18,10 @@ export const Login = () => {
   
   return(
     <div className="login">
-      <input type="text" placeholder="username"/>
+      <input type="email" id="email" placeholder="email" onChange={(event)=>{setUserEmail(event.target.value)}}/>
       <input type="text" placeholder="password" />
-      <button onClick={()=>{getUserId('link@yahoo.com')}}>Submit</button>
+      <button onClick={()=>{console.log(userEmail)}}>Other</button>
+      <button onClick={()=>{getUserId(userEmail)}}>Submit</button>
     </div>
   )
 }
