043159c2a9769fc8397c57b513fa337eaf61faea
Derine Banis <d.banis.13@gmail.com>
2022-08-09 14:28:41 -0700

    Implement expanded view for letters greater than 60 characters

--- client/src/components/LetterListItem.jsx
+++ client/src/components/LetterListItem.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import classNames from "classnames"
 // Material UI
 import Card from '@mui/material/Card';
 import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse"
+import Collapse from "@mui/material/Collapse";
 import { styled } from '@mui/material/styles';
 
 // Material UI Icons
 import IconButton from "@mui/material/IconButton";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExpandCircleDownOutlinedIcon from '@mui/icons-material/ExpandCircleDownOutlined';
 
 const ExpandMore = styled((props) => {
@@ -26,11 +26,22 @@ const ExpandMore = styled((props) => {
 export const LetterListItem = (props) => {
   const navigate = useNavigate();
   const [expanded, setExpanded] = useState(false);
-  const { letter, setCurrentLetter } = props
+  const { letter, setCurrentLetter } = props;
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+
+  const disabled = classNames({"Mui-disabled" : props.letterMessage.length < 60})
+
+  const longLetter = (message) => {
+    if(message.length > 60){
+      return true
+    }
+    else{
+      return false
+    }
+  }
 
   return (
     <Card sx={{ width: 275, margin: 1, padding: 1 }}
@@ -41,12 +52,13 @@ export const LetterListItem = (props) => {
         @import url('https://fonts.googleapis.com/css2?family=Square+Peg&display=swap');
       </style>
       {/* <h5>Hello friend</h5> */}
-      {!expanded && <p className="letterMessage">{props.letterMessage.substring(0, 10)}...</p>}
+      {!expanded && <p className="letterMessage">{props.letterMessage.substring(0, 60)}{longLetter(props.letterMessage) && <span>...</span>}</p>}
+      
       <Collapse in={expanded} timeout="100" unmountOnExit>
         <p className="letterMessage">{props.letterMessage}</p>
       </Collapse>
       <CardActions>
-        <ExpandMore
+        <ExpandMore className={disabled}
           expand={expanded}
           onClick={handleExpandClick}
           aria-expanded={expanded}
@@ -61,5 +73,5 @@ export const LetterListItem = (props) => {
         Type: {props.type}
       </footer>
     </Card>
-  )
-}
+  );
+};
