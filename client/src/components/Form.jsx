f6dc3785dad8547527d95f41d96f1a11f569670f
john <jrborges52@gmail.com>
2022-08-10 12:40:45 -0700

    add clear and submit buttons

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react"
-import { Button } from "./Button"
+// import { Button } from "./Button"
 import { TypeSelector } from "./TypeSelector"
 import { useContext } from "react";
 import { UserContext } from "../UserContext";
 import axios from "axios";
+import "../styles/letterItem.scss";
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import ClearIcon from '@mui/icons-material/Clear';
+
+
+// Mui Components //
+import TextField from '@mui/material/TextField';
 
 export const Form = (props) => {
   const {userID} = useContext(UserContext);
@@ -24,16 +32,44 @@ export const Form = (props) => {
   }
 
   return (
-    <div>
-      <h1>this is a form</h1>
-      <form onChange={(event)=>{setMessage(event.target.value)}} onSubmit={(event)=>{event.preventDefault()}}>
+  <>
+    <div className="text-area-component">
+      {/* <h1>this is a form</h1> */}
+      {/* <form onChange={(event)=>{setMessage(event.target.value)}} onSubmit={(event)=>{event.preventDefault()}}>
         <label>Message</label>
         <textarea name="message" id="message-id" cols="30" rows="10" placeholder="Type Here"></textarea>
-      </form>
-      {console.log(props.letterID)}
-      {!props.isResponse && <TypeSelector onChange={(event) => {setLetterType(event.target.value)}}></TypeSelector>}
-      {!props.isResponse && <Button onClick={()=> {submitMessage(message, letterType, userID); }}>Submit</Button>}
-      {props.isResponse && <Button onClick={()=> {submitResponse(message, props.letterID, userID); }}>Submit</Button>}
+      </form> */}
+
+
+        <TextField  sx={{width: 1 }} style={{marginTop: "40px", marginBottom: "30px" }}
+                  
+                  id="filled-multiline-flexible"
+                  label="Write here"
+                  multiline
+                  minRows={10}
+                  value={message}
+                  onChange={(e) => {setMessage(e.target.value); console.log(message)}}
+                  variant="outlined"
+                />
+
     </div>
+      <div>
+      {!props.isResponse && <TypeSelector onChange={(event) => {setLetterType(event.target.value)}}></TypeSelector>}
+
+      {!props.isResponse && 
+      <span>
+        <Button size="small" variant="outlined" endIcon={<ClearIcon />} onClick={()=> {submitMessage(message, letterType, userID); }}>Clear</Button> 
+        
+        <Button size="small" variant="contained" endIcon={<SendIcon />} onClick={()=> {submitMessage(message, letterType, userID); }}>Submit</Button>
+      </span>}
+
+      {props.isResponse && 
+      <span>
+        <Button size="small" variant="outlined" endIcon={<ClearIcon />} onClick={()=> {submitResponse(message, props.letterID, userID); }}>Clear</Button> 
+      
+        <Button size="small" variant="contained" endIcon={<SendIcon />} onClick={()=> {submitResponse(message, props.letterID, userID); }}>Submit</Button>
+      </span>}
+      </div>
+    </>
   )
 }
