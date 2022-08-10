3d33ca13bb8a522cff5759a4b639e1ffdee28081
john <jrborges52@gmail.com>
2022-08-09 19:53:50 -0700

    refactor the style code

--- client/src/components/LetterListItem.jsx
+++ client/src/components/LetterListItem.jsx
@@ -26,14 +26,14 @@ const ExpandMore = styled((props) => {
 export const LetterListItem = (props) => {
   const navigate = useNavigate();
   const [expanded, setExpanded] = useState(false);
-  const { letter, setCurrentLetter } = props;
+  // const { letter, setCurrentLetter } = props;
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
-  const disabled = classNames({ "Mui-disabled": props.letterMessage.length < 60 });
-  const something = classNames({ 'letterItem': !expanded }, { 'letter-item-vh': expanded });
+  const disabled = classNames( { "Mui-disabled": props.letterMessage.length < 60 });
+  const expandedDiv = classNames("cardStyle",{ 'letterItem': !expanded }, { 'letter-item-vh': expanded });
 
   const longLetter = (message) => {
     if (message.length > 60) {
@@ -44,45 +44,27 @@ export const LetterListItem = (props) => {
     }
   };
 
-  const cardStyle = {
-    width: "85vw",
-    margin: "20px",
-    display: "flex",
-    "flex-direction": "row"
-  };
-
 
   return (
-    <Card className={something}
+    <Card className={expandedDiv}
       sx={{ width: 275, margin: 1, padding: 1 }}
-      style={cardStyle}
+      
     //    onClick={() => setCurrentLetter(letter)}>
     // onClick={() => navigate(`/letters/${letter.id}`)}
     >
-      <style>
-        @import url('https://fonts.googleapis.com/css2?family=Square+Peg&display=swap');
-      </style>
-      {/* <h5>Hello friend</h5> */}
-      <div className='toomany' style={{
-        display: "flex",
-        "flex-direction": "row",
-        "justify-content": "space-between",
-        width: "100%"
-      }}>
+      <div className='letter-wrapper-primary'>
           <div>
             <p>🎈</p>
           </div>
-        <div className='john' style={{ display: 'flex', "flex-direction": "column", width: "90%", "justify-content": "space-between" }}>
-          <div style={{ width: "90%" }}>
+        <div className='letter-wrapper-secondary'>
+          <div className='letter-text-area'>
             {!expanded && <p className="letterMessage">{props.letterMessage.substring(0, 60)}{longLetter(props.letterMessage) && <span>...</span>}</p>}
             <Collapse in={expanded} timeout="100" unmountOnExit>
               <p className="letterMessage">{props.letterMessage}</p>
             </Collapse>
           </div>
-
-        <footer>
-          <div>
-
+        <footer className="letter-footer">
+          <div className="letter-userID"> {/*change here after*/}
             Type: {props.type}
           </div>
           <CardActions>
@@ -96,17 +78,8 @@ export const LetterListItem = (props) => {
             </ExpandMore>
           </CardActions>
         </footer>
-
-
         </div>
       </div>
-
-
-
-
-
-
-
     </Card>
   );
 };
