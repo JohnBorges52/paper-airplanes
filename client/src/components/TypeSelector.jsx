9b273ae781055ae5206a84a3e887cd0ea0ec8df8
Calvin Ho <calvin.ch.ho@gmail.com>
2022-08-10 18:21:30 -0400

    Format type selector in form

--- client/src/components/TypeSelector.jsx
+++ client/src/components/TypeSelector.jsx
@@ -6,35 +6,23 @@ import Select from '@mui/material/Select';
 
 export const TypeSelector = (props) => {
 
-  // const [letterType, setLetterType] = useState("request");
 
-  // const handleChange = (event) => {
-  //   setLetterType(event.target.value);
-  // };
 
   return (
-    // <div className="letter-type-selector">
-    //   <label htmlFor="letterTypes"> Choose type of letter </label>
-    //   <select name="letterTypes" id="letterTypes" onChange={(event) => { props.onChange(event) }}
-
-    // /*onChange={() => {props.onChange()}}*/ >
-
-    //     <option value="request" > Request </option>
-    //     <option value="vent">Vent</option>
-    //     <option value="encourage"> Encourage </option>
-    //   </select>
-    // </div>
 
 
-    <FormControl fullWidth>
-      <InputLabel id="demo-simple-select-label">Letter Type</InputLabel>
+
+    <FormControl fullWidth placeholder="Letter Type">
+      {/* <InputLabel id="demo-simple-select-label">Letter Type</InputLabel> */}
       <Select
         labelId="demo-simple-select-label"
         id="demo-simple-select"
-        // value={letterType}
         label="Letter-Type"
         onChange={event => props.onChange(event)}
       >
+        <MenuItem disabled value="">
+          <em>Select a Letter Type</em>
+        </MenuItem>
         <MenuItem value={"request"}>Request</MenuItem>
         <MenuItem value={"vent"}>Vent</MenuItem>
         <MenuItem value={"encourage"}>Encourage</MenuItem>
