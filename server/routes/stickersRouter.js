15540e9d9f440573c12a5a59327735f8124e0179
Derine Banis <d.banis.13@gmail.com>
2022-08-14 12:38:56 -0700

    Add route to get random sticker

--- server/routes/stickersRouter.js
+++ server/routes/stickersRouter.js
@@ -5,14 +5,10 @@ const router = require("express").Router();
 module.exports = (db) => {
 
   router.get("/", (req, res) => {
-    const queryString = "SELECT * FROM users where email=$1";
-    db.query(queryString, [req.query.email])
+    const queryString = "select * from stickers order by random() limit 1;";
+    db.query(queryString)
       .then((data) => {
         res.json(data.rows);
-      })
-      .then(()=>{
-        console.log(12,req.query.email)
-
       })
   });
 
