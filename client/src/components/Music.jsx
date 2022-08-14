0c429c5a8feb4ae8689eb99254dce217cf7b7ac8
Derine Banis <d.banis.13@gmail.com>
2022-08-14 14:18:41 -0700

    Refactor to use functions

--- client/src/components/Music.jsx
+++ client/src/components/Music.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Card} from "@mui/material"
 import {Button} from "@mui/material";
+import { purple } from "@mui/material/colors";
 
 import song1 from "../assests/test2.mp3"
 import song2 from "../assests/test.mp4"
@@ -8,13 +9,47 @@ import song2 from "../assests/test.mp4"
 export const Music = () => {
 
   const audio = new Audio(song2)
+
+  const playFunc = () => {
+    audio.volume = 0.2
+    audio.play()
+  }
+
+  const pauseFunc = () => {
+    audio.pause()
+  }
   
   
   return(
-    
-    <Card>
-      <Button onClick={()=>{audio.play()}}>Play</Button>
-      <Button onClick={()=>{audio.pause()}}>Pause</Button>
+
+    <Card id="music-controls"
+      sx={{ 
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: 1, 
+        padding: 1, 
+        backgroundColor: purple[100], 
+        marginBottom: "40px", 
+        boxShadow: "7px 7px purple", 
+        border: "1px solid", 
+        borderColor: purple[600] }}
+      >
+        <h1 className="letterListHeader">Music</h1>
+      <Button 
+        variant="outlined"
+        sx={{ width: 0.8 ,color: purple[800], border: "1px solid purple" }} 
+        onClick={()=>{playFunc()}}
+        >
+          Play
+      </Button>
+      <Button 
+        variant="outlined"
+        sx={{ width: 0.8 ,color: purple[800], border: "1px solid purple" }} 
+        onClick={()=>{pauseFunc()}}
+        >
+          Pause
+      </Button>
     </Card>
   )
 }
