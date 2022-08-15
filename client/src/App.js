c7283b653712e1b34a7f97b989c334cb2baeeb2e
Calvin Ho <calvin.ch.ho@gmail.com>
2022-08-15 00:11:37 -0400

    Add link from notification bell to My Letters

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
-import "./styles/musicwidget.scss"
+import "./styles/musicwidget.scss";
 // import "./styles/letterItem.scss";
 // import { Navbar } from "./components/Navbar";
 // import { Form } from "./components/Form";
@@ -26,8 +26,8 @@ import {
 import Paper from "@mui/material/Paper";
 import { purple } from "@mui/material/colors";
 //Music Imports
-import song_one from "./assests/music/song_one.mp3"
-import song_two from "./assests/music/song_two.mp3"
+import song_one from "./assests/music/song_one.mp3";
+import song_two from "./assests/music/song_two.mp3";
 // import song_three from "./assests/music/song_three.mp3"
 
 // Material UI Icons
@@ -36,39 +36,36 @@ import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
 import MarkunreadMailboxOutlinedIcon from "@mui/icons-material/MarkunreadMailboxOutlined";
 import NotificationsActiveOutlinedIcon from "@mui/icons-material/NotificationsActiveOutlined";
 import axios from "axios";
-import io from 'socket.io-client';
+import io from "socket.io-client";
 import { useEffect } from "react";
+
 function App() {
   const navigate = useNavigate();
   const [page, setPage] = useState(1);
   const [value, setValue] = useState("recents");
   const { userID, setUserID } = useContext(UserContext);
-  
-  
+
   const [socket, setSocket] = useState();
-  const [updateNum, setUpdateNum] = useState(0)
-  
-  
-  
+  const [updateNum, setUpdateNum] = useState(0);
+
   useEffect(() => {
     const socket = io();
     setSocket(socket);
-    socket.on('connect', () => {
-      const data = {1:'yes'}
-      console.log('data on client', data);
-      socket.emit('user', data);
+    socket.on("connect", () => {
+      const data = { 1: "yes" };
+      console.log("data on client", data);
+      socket.emit("user", data);
     });
 
-    socket.on('update', ()=>{setUpdateNum(updateNum + 1)})
-
-
+    socket.on("update", () => {
+      setUpdateNum(updateNum + 1);
+    });
 
     // clean up
     return () => {
       socket.disconnect();
     };
   }, [updateNum]);
-
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -79,20 +76,20 @@ function App() {
     setPage(1);
   };
 
-  const songs = [song_one, song_two]
+  // Music widget
+  const songs = [song_one, song_two];
 
-  const audio = new Audio(songs[Math.floor(Math.random() * 2)])
-  const [music] = useState(audio)
-
+  const audio = new Audio(songs[Math.floor(Math.random() * 2)]);
+  const [music] = useState(audio);
 
   const playFunc = (music) => {
-    music.volume = 0.08
-    music.play()
-  }
+    music.volume = 0.08;
+    music.play();
+  };
 
   const pauseFunc = (music) => {
-    music.pause()
-  }
+    music.pause();
+  };
   return (
     <div className="App">
       <nav className="top-nav-bar">
@@ -103,9 +100,13 @@ function App() {
           }}
         ></div>
 
-        <div className="top-nav-bar-rightcontainer">
-          {!userID ?
-
+        <div
+          onClick={() => {
+            navigate("/letters/profile");
+          }}
+          className="top-nav-bar-rightcontainer"
+        >
+          {!userID ? (
             <li>
               <Button
                 variant="outlined"
@@ -117,8 +118,7 @@ function App() {
                 LOGIN
               </Button>
             </li>
-            :
-
+          ) : (
             <>
               <li>
                 <Button
@@ -126,24 +126,20 @@ function App() {
                   sx={{ color: purple[800], border: "1px solid purple" }}
                   onClick={() => {
                     setUserID(null);
-                    navigate("/users/login")
+                    navigate("/users/login");
                   }}
                 >
                   LOGOUT
                 </Button>
               </li>
               <li className="notification-bell">
-
                 <NotificationsActiveOutlinedIcon sx={{ color: purple[700] }} />
                 <NotificationCounter />
               </li>
             </>
-          }
-
-
-
+          )}
         </div>
-      </nav >
+      </nav>
 
       <BottomNavigation
         showLabels
@@ -198,13 +194,23 @@ function App() {
         <Route
           path="/"
           element={
-            <LetterList update={updateNum} page={page} setPage={setPage} path={"/letters"} />
+            <LetterList
+              update={updateNum}
+              page={page}
+              setPage={setPage}
+              path={"/letters"}
+            />
           }
         />
         <Route
           path="/letters"
           element={
-            <LetterList page={page}  update={updateNum} setPage={setPage} path={"/letters"} />
+            <LetterList
+              page={page}
+              update={updateNum}
+              setPage={setPage}
+              path={"/letters"}
+            />
           }
         />
         <Route path="/letters/new" element={<LetterNew />} />
@@ -219,14 +225,20 @@ function App() {
           }
         />
         <Route path="/letters/:id" element={<LetterDetail />} />
-        <Route path="/users/login" element={<Login redirectPath={"/letters/profile"}/>} />
-        <Route path="/users/login/error" element={<LoginError redirectPath={"/letters/profile"}/>} />
-        <Route path="/chill" element={<Music/>} />
+        <Route
+          path="/users/login"
+          element={<Login redirectPath={"/letters/profile"} />}
+        />
+        <Route
+          path="/users/login/error"
+          element={<LoginError redirectPath={"/letters/profile"} />}
+        />
+        <Route path="/chill" element={<Music />} />
       </Routes>
       <div id="music-widget">
         <Music play={playFunc} pause={pauseFunc} music={music}></Music>
       </div>
-    </div >
+    </div>
   );
 }
 
