6dcc1d403d7cd58ee4e7173c8278b38f9776bb1b
Calvin Ho <calvin.ch.ho@gmail.com>
2022-08-15 00:22:19 -0400

    Fix browser warnings due to missing value tag in type and emoji selector

--- client/src/components/EmoteSelector.jsx
+++ client/src/components/EmoteSelector.jsx
@@ -8,27 +8,28 @@ export const EmoteSelector = (props) => {
   return (
     <div className='inputSelector'>
 
-    <FormControl size="small" style={{marginTop: "10px", width: "25vw", maxWidth: "168px", minWidth: "145px" }}>
-      <InputLabel id="letter-type-label" style={{ backgroundColor: "white" }}>I am feeling... &nbsp;</InputLabel>
-      <Select
-        label="Letter-Type"
-        onChange={event => props.onChange(event)}
-      >
-        <MenuItem value={"1"}>🙂</MenuItem>
-        <MenuItem value={"2"}>😃</MenuItem>
-        <MenuItem value={"3"}>🙃</MenuItem>
-        <MenuItem value={"4"}>🥰</MenuItem>
-        <MenuItem value={"5"}>😍</MenuItem>
-        <MenuItem value={"6"}>😐</MenuItem>
-        <MenuItem value={"7"}>😔</MenuItem>
-        <MenuItem value={"8"}>😕</MenuItem>
-        <MenuItem value={"9"}>😭</MenuItem>
-        <MenuItem value={"10"}>😢</MenuItem>
-        <MenuItem value={"11"}>😩</MenuItem>
-        <MenuItem value={"12"}>😠</MenuItem>
-     
-      </Select>
-    </FormControl>
+      <FormControl size="small" style={{ marginTop: "10px", width: "25vw", maxWidth: "168px", minWidth: "145px" }}>
+        <InputLabel id="letter-type-label" style={{ backgroundColor: "white" }}>I am feeling... &nbsp;</InputLabel>
+        <Select
+          label="Emoji-Type"
+          value={""}
+          onChange={event => props.onChange(event)}
+        >
+          <MenuItem value={"1"}>🙂</MenuItem>
+          <MenuItem value={"2"}>😃</MenuItem>
+          <MenuItem value={"3"}>🙃</MenuItem>
+          <MenuItem value={"4"}>🥰</MenuItem>
+          <MenuItem value={"5"}>😍</MenuItem>
+          <MenuItem value={"6"}>😐</MenuItem>
+          <MenuItem value={"7"}>😔</MenuItem>
+          <MenuItem value={"8"}>😕</MenuItem>
+          <MenuItem value={"9"}>😭</MenuItem>
+          <MenuItem value={"10"}>😢</MenuItem>
+          <MenuItem value={"11"}>😩</MenuItem>
+          <MenuItem value={"12"}>😠</MenuItem>
+
+        </Select>
+      </FormControl>
     </div>
 
   )
