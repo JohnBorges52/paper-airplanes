88cf22cc6c3f5d69682b8edc21da2e8e4b38a191
Calvin Ho <calvin.ch.ho@gmail.com>
2022-08-15 00:56:24 -0400

    Fix link from notification bell to My Letters

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,6 @@ import NotificationsActiveOutlinedIcon from "@mui/icons-material/NotificationsAc
 import axios from "axios";
 import io from "socket.io-client";
 import { useEffect } from "react";
-
 function App() {
   const navigate = useNavigate();
   const [page, setPage] = useState(1);
@@ -76,7 +75,6 @@ function App() {
     setPage(1);
   };
 
-  // Music widget
   const songs = [song_one, song_two];
 
   const audio = new Audio(songs[Math.floor(Math.random() * 2)]);
@@ -100,12 +98,7 @@ function App() {
           }}
         ></div>
 
-        <div
-          onClick={() => {
-            navigate("/letters/profile");
-          }}
-          className="top-nav-bar-rightcontainer"
-        >
+        <div className="top-nav-bar-rightcontainer">
           {!userID ? (
             <li>
               <Button
@@ -132,7 +125,12 @@ function App() {
                   LOGOUT
                 </Button>
               </li>
-              <li className="notification-bell">
+              <li
+                className="notification-bell"
+                onClick={() => {
+                  navigate("/letters/profile");
+                }}
+              >
                 <NotificationsActiveOutlinedIcon sx={{ color: purple[700] }} />
                 <NotificationCounter />
               </li>
