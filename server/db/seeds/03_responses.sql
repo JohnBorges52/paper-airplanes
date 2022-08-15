d196e5d38cd456e2ad5726722ad3786296411cbb
Derine Banis <d.banis.13@gmail.com>
2022-08-15 15:53:24 -0700

    Reseed responses

--- server/db/seeds/03_responses.sql
+++ server/db/seeds/03_responses.sql
@@ -1,39 +1,42 @@
+-- Responses to letter 1
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('That''s ok. You did your best. I''m sure it will make for a great story to tell students in the future if you decide to teach/mentor.', 1, 2, 2);
+
+-- Responses to letter 2
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('It''s normal to feel that way when it''s new, but you can do it!', 2, 2, 2);
+
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Try to think of the good things you have in life. You might be feel better.', 2, 2, 2);
+-- Responses to letter 3
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Thanks for the encouragement friend.', 3, 2, 2);
+
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Your words made me happy.', 3, 2, 2);
+
+-- Responses to letter 4
+
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('I''m sorry to hear about that. I went through something similar and going to therapy helped me a lot to deal with my emotions.', 4, 2, 2);
+
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('One of the better things you can do right now is try to spend some time with your friends and loved ones. It''s ok to not be ok.', 4, 2, 2);
+
+-- Responses to letter 5
+    -- RESPONSE 5 IS A VENT NO RESPONSE NEEDED
+
+-- Responses to letter 6 
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Thanks!', 6, 2, 2);
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('💖💖💖💖💖💖💖💖💖💖💖💖💖💖', 6, 2, 2);
+
+-- Responses to letter 7
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Do what''s best for your mental health. There are plenty of people out there and one of them might be your next potential friend.', 7, 2, 2);
+
+-- Responses to Letter 8
+
+INSERT INTO responses (message, letter_id, responder_id, flag_count, read) VALUES ('The best way to overcome a fear is to practice. Start by engaging in small confrontations until your more comfortable.', 8, 2, 2, true);
+
+-- Responses to Letter 9
+INSERT INTO responses (message, letter_id, responder_id) VALUES ('I''m really so sorry to hear that. What are some things you''ve always wanted to do?  Maybe now is the time to do them.', 9, 2);
+
 -- Responses to letter 16
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('It''s normal to feel that way when it''s new, but you can do it!', 16, 2, 2);
 
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Try to think of the good things you have in life. You might be feel better.', 16, 2, 2);
--- Responses to letter 17
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Thanks for the encouragement friend.', 17, 2, 2);
+INSERT INTO responses (message, letter_id, responder_id) VALUES ('Send them memes.', 16, 2);
 
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Your words made me happy.', 17, 2, 2);
-
--- Responses to letter 18
-
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('I''m sorry to hear about that. I went through something similar and going to therapy helped me a lot to deal with my emotions.', 18, 2, 2);
-
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('One of the better things you can do right now is try to spend some time with your friends and loved ones. It''s ok to not be ok.', 18, 2, 2);
-
--- Responses to letter 19
-    -- RESPONSE 19 IS A VENT NO RESPONSE NEEDED
-
--- Responses to letter 20
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Thanks!', 20, 2, 2);
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('💖💖💖💖💖💖💖💖💖💖💖💖💖💖', 20, 2, 2);
-
--- Responses to letter 21
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Do what''s best for your mental health. There are plenty of people out there and one of them might be your next potential friend.', 21, 2, 2);
-
--- Responses to Letter 22
-
-INSERT INTO responses (message, letter_id, responder_id, flag_count, read) VALUES ('The best way to overcome a fear is to practice. Start by engaging in small confrontations until your more comfortable.',22, 2, 2, true);
-
--- Responses to Letter 23
-INSERT INTO responses (message, letter_id, responder_id) VALUES ('I''m really so sorry to hear that. What are some things you''ve always wanted to do?  Maybe now is the time to do them.', 23, 2);
-
--- Responses to letter 15
-
-INSERT INTO responses (message, letter_id, responder_id) VALUES ('Send them memes.', 15, 2);
-
-INSERT INTO responses (message, letter_id, responder_id) VALUES ('Just be there for them when they need you and listen to them when they have to vent.', 15, 2);
+INSERT INTO responses (message, letter_id, responder_id) VALUES ('Just be there for them when they need you and listen to them when they have to vent.', 16, 2);
 
 
