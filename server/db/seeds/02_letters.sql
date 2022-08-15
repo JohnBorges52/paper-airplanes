fcb6f153ff6c6358715129b5ab7739cc34794050
Derine Banis <d.banis.13@gmail.com>
2022-08-15 15:30:42 -0700

    Reseed DB

--- server/db/seeds/02_letters.sql
+++ server/db/seeds/02_letters.sql
@@ -1,46 +1,64 @@
-INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('Sometimes I’m just doing mundane things, like driving somewhere or getting groceries… and I have this overwhelming urge to just cry, sob and break down. I feel like I could fall apart at any given moment but I have to remind myself that it is not the place or time.', 'vent', 1, 3, 7);
-
-INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('When you are joyful, when you say yes to life and have fun and project positivity all around you, you become a sun in the center of every constellation, and people want to be near you.', 'encourage', 2, 2, 4);
-
-INSERT INTO letters (letter_message, type, sender_id, emote) VALUES (' I found out two days ago that my husband has been having an affair. We have two daughters together and have been together for 11 years. The husband of the woman he’s been having an affair with reached out and told me something was going on. We compared stories and it’s even worse than what he initially thought. They’ve hung out together with our kids. What should I do?', 'request', 3, 9);
-
-INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('Sometimes the music I listen to depresses me but I am so addicted to them that I can not stop listening.', 'vent', 2, 2, 7);
-
-INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('Virtually nothing is impossible in this world if you just put your mind to it and maintain a positive attitude.', 'encourage', 1, 4, 2);
-
-INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('I’ve known my friend group for 8 years, but I’m ready to cut them out of my life but it’s extremely hard considering we all still hang out and play games and talk. What approach should I take?.', 'request', 2, 3, 11);
-
-INSERT INTO letters (letter_message, type, sender_id, emote) VALUES ('The cashier at the supermarket didn''t give me the correct change and I did not have the courage to confront her. I feel that I am affraid of confrontation. How could I get better and be more courageous?' , 'request', 1, 10);
-
-
-INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('A positive atmosphere nurtures a positive attitude, which is required to take positive action.', 'encourage', 2, 3, 1);
- 
+-- Letter 1
 INSERT INTO letters (letter_message, type, sender_id, emote) VALUES ('Believe that life is worth living and your belief will help create the fact.', 'encourage', 3, 5);
 
+-- Letter 2
 INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('Inspiration comes from within yourself. One has to be positive. When you’re positive, good things happen.', 'encourage', 1, 2, 12);
 
+-- Letter 3
 INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('Yesterday I was walking with my dog on the street and a group of guys started following me. I was afraid they would do something to me so I started running, but when I looked back to see them they were entering the bus. So probably they were running to not miss the bus. This kind of situation always happens to me. How can I be more fearless? .', 'request', 1, 3, 8);
 
+-- Letter 4
 INSERT INTO letters (letter_message, type, sender_id, emote) VALUES ('I’m in high school and feel like I have no purpose…. This probably is nothing compared to your problems but I just want to get this out… I feel like I have no purpose in life, and if I do it’s only to serve others, never myself. I basically just go to school come home and serve my parents in WHATEVER they want to be done, it’s never weird stuff but it’s just so taxing. I feel I’ve let them take control of me and are manipulating me just to get stuff done, I’m talking with my dad will be right next to the freezer and will ask me to pull something out of it, if I ever tell him to do it himself because he’s right there, he’ll play the victim or yell at me. How do I get out of this situation?', 'request', 1, 12);
 
+-- Letter 5
 INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('Last week I lost 10k dollars in a poker game and up to now I haven''t found the courage to tell my wife!', 'vent', 2, 2, 6);
 
+-- Letter 6
 INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('In every day, there are 1,440 minutes. That means we have 1,440 daily opportunities to make a positive impact.', 'encourage', 1, 3, 4);
 
+
+--Letter 7
 INSERT INTO letters (letter_message, type, sender_id, emote) VALUES ('Everything bad that is happening to you right now is going to pass. Let time heal you!', 'encourage', 5, 5);
 
+-- Letter 8
 INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('I do not like to play videogames but I am affraid if I tell this to my friends they will not be my friends anymore.', 'vent', 2, 2, 8);
 
+-- Letter 9
 INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('I was born in a specific country in Asia and later immigrated to Canada at age 5. Recently, we went back and I really hate it here. I miss my friends, simple things like the snow, and even Walmart.', 'vent', 1, 3, 7);
 
+-- Letter 10
 INSERT INTO letters (letter_message, type, sender_id, emote) VALUES ('Recently, AncestryDNA had a sale. I thought it would be a great gift idea so I bought 6 of them.Today my family got together and I gave my mom, dad, brother, and 2 sisters each a kit. My mom started freaking out. She told us how she didn’t want us to take them because they had unsafe chemicals. We explained to her how there were actually no chemicals, but we could tell she was still flustered. Later she started trying to convince us that only one of us kids need to take it since we will all have the same results and to resell extra kits to save money. Our parents have been fighting upstairs for the past hour, and we are downstairs trying to figure out who has a different dad.', 'vent', 3, 10);
 
+-- Letter 11
 INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('I’m 18 I don’t have a job yet but I’ve been working towards it. I got my driver''s permit and my parents aren’t overly pushy either when it comes to a genuine license or career path thankfully. However, they are very pushy when it comes to my appearance. My mom gives big speeches about how I should accept myself because of how self-conscious I am but I can’t help it.I am obsessed with piercings, sharp eyeliner and fake lashes because I have very short blunt ones. They are not massive but today when I tried to buy myself some fresh lashes my mom dropped everything and made a huge deal about how it makes me look retarded and then questioned if I was a hooker. Am I wrong for complaining?', 'request', 1, 2, 9);
 
+-- Letter 12
 INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('The power for creating a better future is contained in the present moment: You create a good future by creating a good present.', 'encourage', 1, 3, 1);
 
+-- Letter 13
 INSERT INTO letters (letter_message, type, sender_id, emote) VALUES ('Im 15 and my heart just got broken by someone who I love more than anything and anyone in this world. I need advice on how to deal with this and what to do pls pls pls', 'request', 6, 9);
 
+-- Letter 14
 INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('I don''t feel like I''m good enough yet and I don''t know if I ever will be. Maybe I''ll be satisfied when I earn the privilege to judge all. But I cannot judge everything until I finish judging myself.', 'encourage', 1, 3, 4);
 
+-- Letter 15
 INSERT INTO letters (letter_message, type, sender_id, emote) VALUES ('I''m a 19 year old girl and lately I''ve been feeling a bit low about myself. In my life a lot of friends have come and gone, and it has left me wondering if I''m a bad friend. How can I be a better friend?', 'request', 1, 10);
+
+-- Letter 16
+INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('Sometimes I’m just doing mundane things, like driving somewhere or getting groceries… and I have this overwhelming urge to just cry, sob and break down. I feel like I could fall apart at any given moment but I have to remind myself that it is not the place or time.', 'vent', 1, 3, 7);
+--  Letter 17
+INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('When you are joyful, when you say yes to life and have fun and project positivity all around you, you become a sun in the center of every constellation, and people want to be near you.', 'encourage', 2, 2, 4);
+-- Letter 18
+INSERT INTO letters (letter_message, type, sender_id, emote) VALUES (' I found out two days ago that my husband has been having an affair. We have two daughters together and have been together for 11 years. The husband of the woman he’s been having an affair with reached out and told me something was going on. We compared stories and it’s even worse than what he initially thought. They’ve hung out together with our kids. What should I do?', 'request', 3, 9);
+-- Letter 19
+INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('Sometimes the music I listen to depresses me but I am so addicted to them that I can not stop listening.', 'vent', 2, 2, 7);
+-- Letter 20
+INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('Virtually nothing is impossible in this world if you just put your mind to it and maintain a positive attitude.', 'encourage', 1, 4, 2);
+-- Letter 21
+INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('I’ve known my friend group for 8 years, but I’m ready to cut them out of my life but it’s extremely hard considering we all still hang out and play games and talk. What approach should I take?.', 'request', 2, 3, 11);
+
+-- Letter 22
+INSERT INTO letters (letter_message, type, sender_id, emote) VALUES ('The cashier at the supermarket didn''t give me the correct change and I did not have the courage to confront her. I feel that I am affraid of confrontation. How could I get better and be more courageous?' , 'request', 1, 10);
+
+-- Letter 23
+INSERT INTO letters (letter_message, type, flag_count, sender_id, emote) VALUES ('A positive atmosphere nurtures a positive attitude, which is required to take positive action.', 'encourage', 2, 3, 1);
--- server/db/seeds/03_responses.sql
+++ server/db/seeds/03_responses.sql
@@ -1,16 +1,39 @@
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('1It''s normal to feel that way when it''s new, but you can do it!', 1, 2, 2);
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('2It''s normal to feel that way when it''s new, but yo feel that u can do it!', 1, 2, 2);
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('3It''s normal to feel that way when it''s new, but you can d feel that o it!', 1, 2, 2);
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('4It''s normal to feel that way when it''s new, but feel that  you can do it!', 1, 2, 2);
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('5It''s normal to feel that  feel that  feel that  feel that  feel that  feel that way when it''s new, but you can do it!', 1, 2, 2);
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('6It''s normal to feel that way when it''s new, but you can do feel that  it!', 1, 2, 2);
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('7It''s normal to feel that way when it''s ne feel that w, but you can do it!', 1, 2, 2);
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('8It''s normal to feel t feel that  feel that hat way when it''s new, but you can do it!', 1, 2, 2);
+-- Responses to letter 16
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('It''s normal to feel that way when it''s new, but you can do it!', 16, 2, 2);
 
-INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('9It''s normal to feel that way when it''s ne feel that w, but you can do it!', 2, 2, 2);
-INSERT INTO responses (message, letter_id, responder_id, flag_count, read) VALUES ('10It''s normal to feel t feel that  feel that hat way when it''s new, but you can do it!', 2, 2, 2, true);
-INSERT INTO responses (message, letter_id, responder_id) VALUES ('I''m really so sorry to hear that. What are some things you''ve always wanted to do?  Maybe now is the time to do them.', 2, 2);
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Try to think of the good things you have in life. You might be feel better.', 16, 2, 2);
+-- Responses to letter 17
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Thanks for the encouragement friend.', 17, 2, 2);
 
-INSERT INTO responses (message, letter_id, responder_id) VALUES ('Is there somewhere else where you would like to live instead?  If you do, maybe it''s time to make a plan as to how you can get there.', 3, 2);
-INSERT INTO responses (message, letter_id, responder_id) VALUES ('Test 1', 3, 2);
-INSERT INTO responses (message, letter_id, responder_id) VALUES ('Test 2', 3, 2);
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Your words made me happy.', 17, 2, 2);
+
+-- Responses to letter 18
+
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('I''m sorry to hear about that. I went through something similar and going to therapy helped me a lot to deal with my emotions.', 18, 2, 2);
+
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('One of the better things you can do right now is try to spend some time with your friends and loved ones. It''s ok to not be ok.', 18, 2, 2);
+
+-- Responses to letter 19
+    -- RESPONSE 19 IS A VENT NO RESPONSE NEEDED
+
+-- Responses to letter 20
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Thanks!', 20, 2, 2);
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('💖💖💖💖💖💖💖💖💖💖💖💖💖💖', 20, 2, 2);
+
+-- Responses to letter 21
+INSERT INTO responses (message, letter_id, responder_id, flag_count) VALUES ('Do what''s best for your mental health. There are plenty of people out there and one of them might be your next potential friend.', 21, 2, 2);
+
+-- Responses to Letter 22
+
+INSERT INTO responses (message, letter_id, responder_id, flag_count, read) VALUES ('The best way to overcome a fear is to practice. Start by engaging in small confrontations until your more comfortable.',22, 2, 2, true);
+
+-- Responses to Letter 23
+INSERT INTO responses (message, letter_id, responder_id) VALUES ('I''m really so sorry to hear that. What are some things you''ve always wanted to do?  Maybe now is the time to do them.', 23, 2);
+
+-- Responses to letter 15
+
+INSERT INTO responses (message, letter_id, responder_id) VALUES ('Send them memes.', 15, 2);
+
+INSERT INTO responses (message, letter_id, responder_id) VALUES ('Just be there for them when they need you and listen to them when they have to vent.', 15, 2);
+
+
